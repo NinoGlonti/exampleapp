b05864e118b81917263d35774f7f60633c6e0aa9
NinoGlonti <glonti.nin@gmail.com>
2022-12-06 10:51:25 +0400

    general layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,64 @@
+"use client";
+
+import {
+  Form,
+  Button,
+  Typography,
+  Input,
+  Checkbox,
+  message,
+  FormInstance,
+} from "antd";
+import React, { useState, useEffect, useRef } from "react";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  const { Text } = Typography;
+  const formRef = useRef<FormInstance>(null);
+
+  return (
+    <div className="form-container">
+      <div className="text-headers">
+        <Text className="login-header">Log In</Text>
+        <Text className="login-reqs">Fill in required fields to sign in </Text>
+      </div>
+      <div className="form-wrapper">
+        <Form className="login-form" ref={formRef} layout="vertical">
+          <Form.Item
+            label="Username"
+            hasFeedback
+            name="username"
+            className="form-labels"
+          >
+            <Input
+              name="username"
+              className="form-inputs"
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            label="Password"
+            hasFeedback
+            name="password"
+            className="form-labels"
+          >
+            <Input
+              name="passwoord"
+              className="form-inputs"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Button
+            type="primary"
+            size="large"
+            className="login-btn"
+            onClick={() => {
+              formRef?.current?.submit();
+            }}
+          >
+            Log In
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
